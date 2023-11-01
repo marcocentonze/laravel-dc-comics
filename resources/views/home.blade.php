7c7799bf8ecd094b343171cc41fa9b5dea632ab6
marcocentonze <marco.centonze27@gmail.com>
2023-11-01 11:42:58 +0100

    add writers and artists on page(using collapse)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,8 +10,8 @@
         <div class="row">
             @foreach ($comics as $comic)
                 <div class="col-6 col-md-4 col-lg-3 g-3">
-                    <div class="card h-100">
-                        <img class="card-img-top" src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+                    <div class="card h-100" data-bs-toggle="collapse" data-bs-target="#info-{{ $loop->index }}" aria-expanded="false">
+                        <img class="card-img-top" src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}" style="width: 100%; height: 475px; object-fit: cover;">
                         
                         <div class="card-body">
                             <h4 class="card-title">{{ $comic['title'] }}</h4>
@@ -25,11 +25,31 @@
                                 <span>{{ $comic['type'] }}</span>
                             </div>
                         </div>
+    
+                        <!-- Collapse section (with boostrap),al click sulla card appaiono writers e artists-->
+                        <div class="collapse" id="info-{{ $loop->index }}">
+                            <div class="card-body">
+                                <h5>Writers:</h5>
+                                <ul>
+                                    @foreach($comic['writers'] as $writer)
+                                        <li>{{ $writer }}</li>
+                                    @endforeach
+                                </ul>
+    
+                                <h5>Artists:</h5>
+                                <ul>
+                                    @foreach($comic['artists'] as $artist)
+                                        <li>{{ $artist }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
             @endforeach
         </div>
     </div>
+    
     
     
 @endsection
