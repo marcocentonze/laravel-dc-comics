2adfca4a63d4a56f3dc94c2eb33942aceda459c9
marcocentonze <marco.centonze27@gmail.com>
2023-11-03 16:45:50 +0100

    add delete funcionality with boostrap modal

--- resources/views/admin/comics/index.blade.php
+++ resources/views/admin/comics/index.blade.php
@@ -6,9 +6,15 @@
 
     <div class="container mt-5">
         <h1 class="mb-4">Comics Admin Index</h1>
-         
-         {{-- alert quando è vuota  --}}
-        @if($comics->isEmpty())
+
+        @if (session('message'))
+            <div class="alert alert-success" role="alert">
+                <strong>Success!</strong> {{ session('message') }}
+            </div>
+        @endif
+
+        {{-- alert quando è vuota  --}}
+        @if ($comics->isEmpty())
             <div class="alert alert-warning" role="alert">
                 No comics here!
             </div>
@@ -32,17 +38,60 @@
                                 <td>{{ $comic['title'] }}</td>
                                 <td>
                                     @if (str_contains($comic['thumb'], 'http'))
-                                        <img style="width:50px" class="img-fluid rounded-circle" src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+                                        <img style="width:50px" class="img-fluid rounded-circle" src="{{ $comic['thumb'] }}"
+                                            alt="{{ $comic['title'] }}">
                                     @else
-                                        <img style="width:50px" class="img-fluid rounded-circle" src="{{ asset('storage/' . $comic->thumb) }}" alt="{{ $comic['title'] }}">
+                                        <img style="width:50px" class="img-fluid rounded-circle"
+                                            src="{{ asset('storage/' . $comic->thumb) }}" alt="{{ $comic['title'] }}">
                                     @endif
                                 </td>
                                 <td>{{ $comic['price'] }}</td>
                                 <td>{{ $comic['series'] }}</td>
                                 <td>
-                                    <a href="{{ route('comics.show', $comic->id) }}" class="btn btn-sm btn-secondary">More</a>
+                                    <a href="{{ route('comics.show', $comic->id) }}"
+                                        class="btn btn-sm btn-secondary">More</a>
                                     <a href="{{ route('comics.edit', $comic->id) }}" class="btn btn-sm btn-info">Edit</a>
-                                    <a href="#" class="btn btn-sm btn-danger">Delete</a>
+
+                                    {{-- modal for delete button --}}
+                                    <button type="button" class="btn btn-danger" data-bs-toggle="modal"
+                                        data-bs-target="#modalId-{{ $comic->id }}">
+                                        Delete
+                                    </button>
+
+                                    <!-- Modal Body -->
+                                    <!-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard -->
+                                    <div class="modal fade" id="modalId-{{ $comic->id }}" tabindex="-1"
+                                        data-bs-backdrop="static" data-bs-keyboard="false" role="dialog"
+                                        aria-labelledby="modalTitle-{{ $comic->id }}" aria-hidden="true">
+                                        <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm"
+                                            role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title" id="modalTitle-{{ $comic->id }}">Modal id:
+                                                        {{ $comic->id }}</h5>
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                        aria-label="Close"></button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    Attention! This is a destructive operation that cannot be undone.
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary"
+                                                        data-bs-dismiss="modal">Close</button>
+
+                                                    <!-- Delete form -->
+                                                    <form action="{{ route('comics.destroy', $comic->id) }}"
+                                                        method="POST">
+                                                        @csrf
+                                                        @method('DELETE')
+                                                        <button type="submit" class="btn btn-danger">Confirm</button>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+
                                 </td>
                             </tr>
                         @endforeach
